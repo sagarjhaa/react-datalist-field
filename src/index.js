9f0a2ed323f43c81afd3664e5e754599bc461a2d
Sagar Jha <sagar.jhaa@gmail.com>
2020-03-13 12:49:56 -0700

    5 special character search (#10)
    * Update gitignore
    
    * Update packages.
    
    * Comment out plugin
    
    * Add support to check for parentheses
    
    * update example
    
    * Upadate version to 20.3.0

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import DataList from './DataList';
 
 var cars = [
-  { id: 1, model: "CRV", company: "Honda" },
+  { id: 1, model: "(CRV", company: "Honda" },
   { id: 2, model: "Accord", company: "Honda" },
   { id: 3, model: "800", company: "Maruti" },
   { id: 4, model: "Civic", company: "Honda" },
@@ -14,9 +14,9 @@ var cars = [
   { id: 9, model: "Rav4", company: "Toyota" },
   { id: 10, model: "Camry", company: "Toyota" },
   { id: 11, model: "Innova", company: "Toyota" },
-  { id: 12, model: "Yaris", company: "Toyota" },
-  { id: 13, model: "Prius", company: "Toyota" },
-  { id: 14, model: "Highlander", company: "Toyota" },
+  { id: 12, model: "Ya(ris", company: "Toyota" },
+  { id: 13, model: "Pri(us", company: "Toyota" },
+  { id: 14, model: "High(lander", company: "Toyota" },
   { id: 15, model: "Grand Cherokee", company: "Jeep" },
   { id: 16, model: "Wrangler", company: "Jeep" },
   { id: 17, model: "Comanche", company: "Jeep" }
